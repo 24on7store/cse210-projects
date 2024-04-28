3498cc78984f3a698aa31b5fea39f2fb3117081a
24on7store <132136928+24on7store@users.noreply.github.com>
2024-04-29 02:52:56 +0300

    Week1  Preparations

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("");
+
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != MagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (MagicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (MagicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it, Congrats!!!!");
+            }
+
+            Console.WriteLine("");
+        }
+
+
     }
 }
